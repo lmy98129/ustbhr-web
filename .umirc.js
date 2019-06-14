03920ed18aa4812f7874c565f1bff473715418da
blean <blean@mbp.local>
2019-06-15 00:50:00 +0800

    chore:add .umirc.js and other config files

---
+++ .umirc.js
@@ -0,0 +1,33 @@
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: false,
+      title: 'USTB考勤',
+      dll: false,
+      
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+
+    }],
+  ],
+
+  proxy: {
+    '/api': {
+      target: 'http://localhost:8080',
+      pathRewrite: { '^/api': '' },
+      changeOrigin: true
+    }
+  },
+}
